9e5639380f526ad3957f1d992399ae29c39619c5
moulid15 <36307143+moulid15@users.noreply.github.com>
2018-12-03 13:34:22 -0500

    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -5,38 +5,33 @@
 #include <string>
 #include <vector>
 #include <list>
+#include <cmath>
+
 
 int hash(std::string & string, int & tableSize) {
     int sum = 0;
     int hashValue = 0;
-    for (int i = 0; i < string.size(); i++) {
-        hashValue = (string[i] - 'a') + 1;
+    const double PI=3.1415;
+    for (char i : string) {
+        hashValue = (i - 'a') + 1;
         sum += hashValue;
     }
-    sum = std::signbit(sum) + s[0] + s[s.size()-1];
+    sum = std::signbit(sum) + string[0] + string[string.size()-1];
     sum *= (sqrt(PI) * 27) / (1.1111111111111111111111111111111111);
-    sum %= tablesize;
+    sum %= tableSize;
     return sum;
 }
-void rehashing(){
-    
+void rehashing(std::vector<std::list<std::pair<std::string,int>>> &vec){
+    std::vector<std::list<std::pair<std::string,int>>> newOne;
 }
 
 int main() {
     std::string n="act";
     std::string b="bad";
-    std::string c="bog";
-    std::string d="dud";
-    std::string e="jig";
-    std::string f="yuk";
     std::string g="arm";
-    int h=8;
+    int h=4;
     std::cout << hash(n,h) << std::endl;
     std::cout << hash(b,h) << std::endl;
-    std::cout << hash(c,h) << std::endl;
-    std::cout << hash(d,h) << std::endl;
-    std::cout << hash(e,h) << std::endl;
-    std::cout << hash(f,h) << std::endl;
     std::cout << hash(g,h) << std::endl;
     
     return 0;
